c55f0a96075487ce760733fe5e6c9e53e17f7826
Long <longvuong@live.com>
2025-03-26 05:57:25 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+
+# # A very simple Flask Hello World app for you to get started with...
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello from Flask! This is where I will be doing all my Toronto Hydro charting magic'
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
