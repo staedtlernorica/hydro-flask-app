7cd3eb8400d092771eea8bb863c743f0b0682363
unknown <longvuong@live.com>
2025-04-10 02:36:57 -0400

    fixed non-appearing chart; refactored merge_files function; refactored app.py

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ from wtforms import MultipleFileField, SubmitField
 from werkzeug.utils import secure_filename
 import os, sys
 from wtforms.validators import InputRequired
-
+from viz.extras import merge_files  
+from viz.build_viz import build_viz
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
 app.config['UPLOAD_FOLDER'] = 'static/files'
 app.config['ALLOWED_EXTENSIONS'] = {'xml'}
+upload_path = f'{os.getcwd()}/{app.config['UPLOAD_FOLDER']}'
 
 class UploadFileForm(FlaskForm):
     file = MultipleFileField("Files", validators=[InputRequired()])
@@ -21,35 +23,11 @@ def home():
     form = UploadFileForm()
     if form.validate_on_submit():
         
-        files_filenames = []
-        merged_files = []
         for file in form.file.data:
             secured_filename = secure_filename(file.filename)
-            file.save(os.getcwd() + '/' + os.path.join(app.config['UPLOAD_FOLDER'], secured_filename))
-            # files_filenames.append(file_filename)
+            file.save(f'{upload_path}/{secured_filename}')
+            merged_files = merge_files(upload_path)
 
-            from viz.extras import parse_xml  
-            import xmltodict, json  
-            with open(os.getcwd() + '/' + app.config['UPLOAD_FOLDER'] + '/' + file.filename) as xml_file:
-                # merged_files.append(parse_xml(xml_file))
-                data_dict = xmltodict.parse(xml_file.read())
-                json_data = json.dumps(data_dict)
-
-                for i in range(4, len(data_dict['feed']['entry'])):
-                    metadata = data_dict['feed']['entry'][i]['content']['espi:IntervalBlock']['espi:interval']
-                    readings = data_dict['feed']['entry'][i]['content']['espi:IntervalBlock']['espi:IntervalReading']
-                    day_start = metadata['espi:start']
-
-                    for hourly_readings in readings:       
-                        reading_start = hourly_readings['espi:timePeriod']['espi:start']
-                        reading_value = hourly_readings['espi:value']
-                        merged_files.append({
-                            'day (unix)': day_start,
-                            'hour (unix)': reading_start,
-                            'reading': reading_value
-                        })
-
-        from viz.build_viz import build_viz
         plot_html = build_viz(merged_files)
         return render_template("result.html", plot_html=plot_html)
     
--- viz/extras.py
+++ viz/extras.py
@@ -1,59 +1,27 @@
-import os
-print(os.curdir)
-print(os.listdir())
+def merge_files(file_path):
+    import os, xmltodict
+    files = os.listdir(file_path)
+    merged_readings = []
 
-def parse_xml(xml_file):
-    import xmltodict, json
-    data_dict = xmltodict.parse(xml_file.read())
-    json_data = json.dumps(data_dict)
-    parsed = []
+    for i in files:
+        with open(f'{file_path}/{i}') as xml_file:
+            data_dict = xmltodict.parse(xml_file.read())
 
-    for i in range(4, len(data_dict['feed']['entry'])):
-        metadata = data_dict['feed']['entry'][i]['content']['espi:IntervalBlock']['espi:interval']
-        readings = data_dict['feed']['entry'][i]['content']['espi:IntervalBlock']['espi:IntervalReading']
-        day_start = metadata['espi:start']
+        for i in range(4, len(data_dict['feed']['entry'])):
+            metadata = data_dict['feed']['entry'][i]['content']['espi:IntervalBlock']['espi:interval']
+            readings = data_dict['feed']['entry'][i]['content']['espi:IntervalBlock']['espi:IntervalReading']
+            day_start = metadata['espi:start']
 
-        for hourly_readings in readings:       
-            reading_start = hourly_readings['espi:timePeriod']['espi:start']
-            reading_value = hourly_readings['espi:value']
-            parsed.append({
-                'day (unix)': day_start,
-                'hour (unix)': reading_start,
-                'reading': reading_value
-            })
-            
-    return parsed
+            for hourly_readings in readings:       
+                reading_start = hourly_readings['espi:timePeriod']['espi:start']
+                reading_value = hourly_readings['espi:value']
+                merged_readings.append({
+                    'day (unix)': day_start,
+                    'hour (unix)': reading_start,
+                    'reading': reading_value
+                })
 
-# def merge_readings(*dir):
-#     import os
-#     files = os.listdir('./static/files/')
-#     # files = os.listdir(os.curdir)
-#     x = list(filter(lambda x: 'TH_Electric_Usage_' in x, files))
-#     # print(args)
-#     print('THE FILES ARE ', files)
-
-#     import xmltodict, json
-#     merged_readings = []
-
-#     with open(f'{dir}/{file_name}') as xml_file:
-#         data_dict = xmltodict.parse(xml_file.read())
-#         json_data = json.dumps(data_dict)
-        
-#         for i in range(4, len(data_dict['feed']['entry'])):
-#             metadata = data_dict['feed']['entry'][i]['content']['espi:IntervalBlock']['espi:interval']
-#             readings = data_dict['feed']['entry'][i]['content']['espi:IntervalBlock']['espi:IntervalReading']
-#             day_start = metadata['espi:start']
-
-#             for hourly_readings in readings:       
-#                 reading_start = hourly_readings['espi:timePeriod']['espi:start']
-#                 reading_value = hourly_readings['espi:value']
-#                 merged_readings.append({
-#                     'day (unix)': day_start,
-#                     'hour (unix)': reading_start,
-#                     'reading': reading_value
-#                 })
-
-#     return merged_readings
+    return merged_readings
 
 def assign_season(date):
     import datetime
